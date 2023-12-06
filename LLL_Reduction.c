fd556941b5920ec542b6a4b8057bf0daf9afcb03
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-06 12:25:51 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -108,16 +108,4 @@ void LLL(int count, double delta, int dim, double A[][dim], double B[][dim], ...
   
   
 
-int main() {
-  double A[3][3];
-  double B[3][3];
-  LLL(3, 3, 0.75, A, B, (int[]){1, 1, 1}, (int[]){-1, 0, 2}, (int[]){3, 5, 6});
-  printf("Orthonormalized Vectors (A):\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%.4f\t", A[i][j]);
-        }
-        printf("\n");
-    }
-  return 0;
-}
+
