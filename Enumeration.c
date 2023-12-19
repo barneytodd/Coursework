2c9531bc443388e35d01c5a5c5919467f3a53356
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-19 18:59:15 +0000

    Update Enumeration.c

--- Enumeration.c
+++ Enumeration.c
@@ -1,8 +1,11 @@
 double ShortestVector(int dim, double (*A)[dim]) {
-  bool boolarray[dim];
+  bool boolarray[dim]; //checks if each vector has been changed
   for (i=0, i<dim, i++) {
-    boolarray[i] = True;
-  
+    boolarray[i] = True; //initialises boolarray to all True
+  }
+  double temp_vector[dim]
+  for (i=0, i<dim, i++) {
+    
   }
 }
   
