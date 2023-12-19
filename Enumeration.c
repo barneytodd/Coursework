2f2f87fb088a3a6e60b6a5032d070d650a4bd40e
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-19 21:59:35 +0000

    Update Enumeration.c

--- Enumeration.c
+++ Enumeration.c
@@ -1,3 +1,12 @@
+double VectorNorm(int dim, int *arr) {
+  double sum1;
+  int i;
+  for (i=0, i<dim, i++) {
+    sum1 += arr[i]*arr[i];
+  }
+  return sqrt(sum1);
+}
+
 double ShortestVector(int dim, double (*A)[dim]) {
   bool boolarray[dim]; //checks if each vector has been changed
   for (i=0, i<dim, i++) {
@@ -7,7 +16,7 @@ double ShortestVector(int dim, double (*A)[dim]) {
   for (i=0, i<dim, i++) {
     for (j=i, j<dim, j++) {
       if (i!=j && boolarray[j]) {
-        double i_length;
+        double i_length = VectorNorm(dim, A[][i]);
         double j_lenght;
         
       }
