414c3ac885721c48a7fe6d0fdb13f52ff906a1ce
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-23 14:30:50 +0000

    Update Enumeration.c

--- Enumeration.c
+++ Enumeration.c
@@ -1,1 +1,28 @@
+#include <string.h>
 
+double InnerProduct(int dim, int *arr1, int *arr2) {
+  double sum1;
+  int i;
+  for (i=0, i<dim, i++) {
+    sum1 += arr1[i]*arr2[i];
+  }
+  return sqrt(sum1);
+}
+
+bool EndCheck(int dim, int current_vec[dim], double (*A)[dim]) {
+  //no minuses of ones already there
+  //if all possible next vectors have dot product > 90 degrees, end
+  //no repeats of what we've just tried
+  //possibly have a dot product lookup_table
+  bool possible_vec_check = 
+}
+
+double ShortestVector(int dim, double (*A)[dim]) { //A is  amtrix of row vectors stacked
+  //create an int array to keep track of current vector
+  //define end points
+  //do an initial branch
+  //enumerate out to in
+  //keep track of shortest vector
+  int current_vec[dim];
+  
+}
