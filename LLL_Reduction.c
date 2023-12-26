e31c1c54279d660fbd7ea1964c9cfcb05d20cde8
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-26 18:33:11 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -13,14 +13,14 @@ double InnerProduct(int dim, int *arr1, int *arr2) {
   return sqrt(sum1);
 }
 
-void GramSchmidt(int dim, double (*A)[dim] {
+void GramSchmidt(int dim, int start, double (*A)[dim]) {
   int i, j, k; //initialise variables i, j, k
   double mu_ij;
   double vec1[dim];
   for (i=0; i<dim; i++) {
     vec1[i] = 0
   }
-  for (i=0; i<dim; i++) { //iterate through the variables (vectors)
+  for (i=start; i<dim; i++) { //iterate through the variables (vectors)
     for (j=0; j<i; j++) { //iterate through the previous vectors
       mu_ij = InnerProduct(dim, A[i], A[j])/InnerProduct(dim, A[j], A[j]); 
       for (k=0; k<dim; k++) {
@@ -34,7 +34,7 @@ void GramSchmidt(int dim, double (*A)[dim] {
 }
   
 
-void update_matrices(int count, int dim, double A[][count], double B[][count], double M[][count]) {
+void update_matrices(int dim, int start, double (*A)[dim], double B[][count]) {
   int i, j, k;
   for (i=0; i<count; i++) {
     for (j=0; j<dim; j++) {
@@ -42,17 +42,17 @@ void update_matrices(int count, int dim, double A[][count], double B[][count], d
     }
   }
   GramSchmidt(count, dim, B);
-  for (i=0; i<count; i++) { 
-    for (j=0; j<count; j++) {
-      double inner_product1 = 0.0;
-      double inner_product2 = 0.0;
-      for (k=0; k<dim; k++) {
-        inner_product1 += A[k][i] * B[k][j];
-        inner_product2 += B[k][j] * B[k][j];
-      }
-      M[i][j] = inner_product1/inner_product2; // calculate components of M
-    }
-  }
+  //for (i=0; i<count; i++) { 
+  //  for (j=0; j<count; j++) {
+  //    double inner_product1 = 0.0;
+  //    double inner_product2 = 0.0;
+  //    for (k=0; k<dim; k++) {
+  //      inner_product1 += A[k][i] * B[k][j];
+  //      inner_product2 += B[k][j] * B[k][j];
+  //    }
+  //    M[i][j] = inner_product1/inner_product2; // calculate components of M
+  //  }
+  //}
 }
 
 
@@ -60,130 +60,136 @@ void update_matrices(int count, int dim, double A[][count], double B[][count], d
 void LLL(double delta, int dim, double (*A)[dim], double B[][dim], ...) {
   va_list ap; //initialise list of variables
   int i, j, k, m; //initialise variables i, j, k
+  double *vector
   va_start (ap, B); //initialise va_list
-  for (i=0; i<count; i++) { //iterate through the variables (vectors)
-    int *vector = va_arg (ap, int*); //store the vector in the variable vector
+  for (i=0; i<dim; i++) { //iterate through the variables (vectors)
+    vector = va_arg (ap, *double); //store the vector in the variable vector
     for (k=0; k<dim; k++) { 
-      A[k][i] = vector[k]; // initialise row i of A
-      B[k][i] = vector[k]; // initialise to be the same as A
+      A[i][k] = vector[k]; // initialise row i of A
+      B[i][k] = vector[k]; // initialise to be the same as A
       } 
   }
-  printf("A, B after initialisation:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("A: %.4f\t", A[i][j]);
-        }
-        printf("\n");
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("B: %.4f\t", B[i][j]);
-        }
-        printf("\n");
-    }
-  GramSchmidt(count, dim, B); //GramSchmidt B
-  printf("A, B after GramSchmidt:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("A: %.4f\t", A[i][j]);
-        }
-        printf("\n");
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("B: %.4f\t", B[i][j]);
-        }
-        printf("\n");
-    }
-  double M[count][count]; // initialise a new matrix M
-  for (i=0; i<count; i++) { 
-    for (j=0; j<count; j++) {
-      double inner_product1 = 0.0;
-      double inner_product2 = 0.0;
-      for (k=0; k<dim; k++) {
-        inner_product1 += A[k][i] * B[k][j];
-        inner_product2 += B[k][j] * B[k][j];
-      }
-      M[i][j] = inner_product1/inner_product2; // calculate components of M
-    }
-  }
-  printf("M after initialisation:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("%.4f\t", M[i][j]);
-        }
-        printf("\n");
-    }
-  k = 0;
+  //printf("A, B after initialisation:\n");
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("A: %.4f\t", A[i][j]);
+        //}
+        //printf("\n");
+    //}
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("B: %.4f\t", B[i][j]);
+        //}
+        //printf("\n");
+    //}
+  GramSchmidt(dim, 0, B); //GramSchmidt B
+  //printf("A, B after GramSchmidt:\n");
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("A: %.4f\t", A[i][j]);
+        //}
+        //printf("\n");
+    //}
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("B: %.4f\t", B[i][j]);
+        //}
+        //printf("\n");
+    //}
+  //double M[count][count]; // initialise a new matrix M
+  //for (i=0; i<count; i++) { 
+    //for (j=0; j<count; j++) {
+      //double inner_product1 = 0.0;
+      //double inner_product2 = 0.0;
+      //for (k=0; k<dim; k++) {
+        //inner_product1 += A[k][i] * B[k][j];
+        //inner_product2 += B[k][j] * B[k][j];
+      //}
+      //M[i][j] = inner_product1/inner_product2; // calculate components of M
+    //}
+  //}
+  //printf("M after initialisation:\n");
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("%.4f\t", M[i][j]);
+        //}
+        //printf("\n");
+    //}
+  k = 1;
+  double mu_kj;
+  double mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]);
   while (k<dim) {
     for (j=k-1; j>=0; j--) {
-      if (fabs(M[k][j]) > 1/2) {
-        int Mint = round(M[k][j]);
+      mu_kj = InnerProduct(dim, A[k], B[j])/InnerProduct(dim, B[j], B[j]); 
+      if (fabs(mu_kj) > 1/2) {
+        //int Mint = round(M[k][j]);
         for (i=0; i<dim; i++) {
-          A[i][k] -= Mint * A[i][j];
+          A[k][i] -= round(mu_kj) * A[j][i];
         }
-        update_matrices(count, dim, A, B, M);
+        update_matrices(dim, k, A, B);
       }
     }
-    printf("A, B, M after updating:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("A: %.4f\t", A[i][j]);
-        }
-        printf("\n");
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("B: %.4f\t", B[i][j]);
-        }
-        printf("\n");
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("M: %.4f\t", M[i][j]);
-        }
-        printf("\n");
-    }
-    double inner_product1 = 0.0;
-    double inner_product2 = 0.0;
-    for (i=0; i<dim; i++) {
-      inner_product1 += B[i][k]*B[i][k];
-      printf("B[i][k]: %.4f, i: %d, k: %d\n", B[i][k], i, k);
-      inner_product2 += B[i][k-1]*B[i][k-1];
-      }
-    printf("Inner product 1: %.4f\n", inner_product1);
-    printf("Compare to: %.4f\n", ((delta - (M[k][k-1]*M[k][k-1])) * inner_product2));
-    if (inner_product1 > ((delta - (M[k][k-1]*M[k][k-1])) * inner_product2)) {
+    //printf("A, B, M after updating:\n");
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("A: %.4f\t", A[i][j]);
+        //}
+        //printf("\n");
+    //}
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("B: %.4f\t", B[i][j]);
+        //}
+        //printf("\n");
+    //}
+    //for (int i = 0; i < 3; i++) {
+        //for (int j = 0; j < 3; j++) {
+            //printf("M: %.4f\t", M[i][j]);
+        //}
+        //printf("\n");
+    //}
+    //double inner_product1 = 0.0;
+    //double inner_product2 = 0.0;
+    //for (i=0; i<dim; i++) {
+      //inner_product1 += B[i][k]*B[i][k];
+      //printf("B[i][k]: %.4f, i: %d, k: %d\n", B[i][k], i, k);
+      //inner_product2 += B[i][k-1]*B[i][k-1];
+      //}
+    //printf("Inner product 1: %.4f\n", inner_product1);
+    //printf("Compare to: %.4f\n", ((delta - (M[k][k-1]*M[k][k-1])) * inner_product2));
+    //mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
+    if (InnerProduct(dim, B[k], B[k]) > ((delta - (mu_k_kminus1*mu_k_kminus1)) * InnerProduct(dim, B[k-1], B[k-1]))) {
       k+=1;
+      mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
         }
       else {
-        double temp[dim];
         for (i=0; i<dim; i++) {
-          temp[i] = A[i][k];
-          A[i][k] = A[i][k-1];
-          A[i][k-1] = temp[i];
+          A[i][k] ^= A[i][k-1];
+          A[i][k-1] ^= A[i][k];
+          A[i][k] ^= A[i][k-1];
         }
-        update_matrices(count, dim, A, B, M); 
-        printf("A, B, M after updating again:\n");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("A: %.4f\t", A[i][j]);
-            }
-            printf("\n");
-        }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("B: %.4f\t", B[i][j]);
-            }
-            printf("\n");
-        }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("M: %.4f\t", M[i][j]);
-            }
-            printf("\n");
-        }
-        k = fmax(k-1, 1); 
+        update_matrices(dim, k, A, B); 
+        //printf("A, B, M after updating again:\n");
+        //for (int i = 0; i < 3; i++) {
+            //for (int j = 0; j < 3; j++) {
+                //printf("A: %.4f\t", A[i][j]);
+            //}
+            //printf("\n");
+        //}
+        //for (int i = 0; i < 3; i++) {
+            //for (int j = 0; j < 3; j++) {
+                //printf("B: %.4f\t", B[i][j]);
+            //}
+            //printf("\n");
+        //}
+        //for (int i = 0; i < 3; i++) {
+            //for (int j = 0; j < 3; j++) {
+                //printf("M: %.4f\t", M[i][j]);
+            //}
+            //printf("\n");
+        //}
+        k = fmax(k-1, 1);
+        mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]);           
       }
   }
   va_end(ap);
