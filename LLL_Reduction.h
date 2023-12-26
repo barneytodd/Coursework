3d7a2b734da13e09dbf71cbce3c118e1aeaf2e15
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-26 19:13:40 +0000

    Update LLL_Reduction.h

--- LLL_Reduction.h
+++ LLL_Reduction.h
@@ -1,5 +1,5 @@
-void LLL(int count, double delta, int dim, double A[][dim], double B[][dim], ...);
+void LLL(double delta, int dim, double (*A)[dim] ...);
 
-void GramSchmidt(int count, int dim, double A[][dim]);
+void GramSchmidt(int dim, int start, double (*A)[dim]);
 
-void update_matrices(int count, int dim, double A[][dim], double B[][dim], double M[][count]);
+void update_matrices(int dim, int start, double (*A)[dim], double B[][dim]);
