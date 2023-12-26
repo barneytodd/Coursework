09179e5e9e611d8963cec047382edcb4c9f773dd
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-26 19:48:10 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int main() {
   double v1[3] = {1.0, 0.0, 0.0};
   double v2[3] = {0.0, 0.1, 0.0};
   double v3[3] = {0.0, 0.0, 0.1};
-  double *vec1 = &v1;
-  double *vec2 = &v2;
-  double *vec3 = &v3;
+  double *vec1[3] = &v1;
+  double *vec2[3] = &v2;
+  double *vec3[3] = &v3;
   LLL(0.75, 3, A, vec1, vec2, vec3);
   printf("Orthonormalized Vectors (A):\n");
     for (int i = 0; i < 3; i++) {
