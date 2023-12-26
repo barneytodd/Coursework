9757bc383b9e9213bb04395d8344dce938dd3422
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-26 19:39:35 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 
 int main() {
   double A[3][3];
-  double B[3][3];
-  LLL(0.75, 3, A, B, (int[]){1, 0, 0}, (int[]){0, 1, 0}, (int[]){0, 0, 1});
+  LLL(0.75, 3, A, (int[]){1, 0, 0}, (int[]){0, 1, 0}, (int[]){0, 0, 1});
   printf("Orthonormalized Vectors (A):\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
