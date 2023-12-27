8681d032aeb9001d66751cb6fdf782ba260f0667
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-27 20:09:45 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,41 @@
 #include "LLL_Reduction.h"
 #include "Enumeration.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  double A[3][3];
-  
+  int dim, i, j, k;
   printf("You have entered %d arguments:\n", argc);
-  for (int i = 0; i < argc; i++) {
-    if (argv[i][0] == '[') {
-      printf("i: %d\n", i);
+  for (i = 2; i < argc; i++) {
+    if (argv[i][0] == '[' && i!=1) {
+      dim = i-1;
+      break;
     }
-      printf("%s\n", argv[i]);
   }
+  double A[dim][dim];
+  for (i = 0; i < dim; i++) {
+    for (j=0; j < dim; j++) {
+      k = 1 + dim*i + j;
+      if (argv[k][0] == '[') {
+        A[i][j] = strtod(&argv[k][1], Null); //Null might need to be changed here
+      }
+      if (argv[k][-1] == ']' {
+        A[i][j] = strtod(&argv[k][-1], Null);
+      }
+      else {
+        A[i][j] = strtod(argv[k], Null);
+      }
+    }
+  }
+  printf("A\n");
+  for (i = 0; i < dim; i++) {
+    for (j=0; j < dim; j++) {
+      printf("%.4f\n", A[i][j]);
+    }
+  }
+  printf("\n");
   
+      
   double vec1[3] = {1.0, 1.0, 1.0};
   double vec2[3] = {-1.0, 0.0, 2.0};
   double vec3[3] = {3.0, 5.0, 6.0};
