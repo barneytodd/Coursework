fd22a6536aa5b8d2e9d221d5deb23ba5eee24b66
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-27 00:28:15 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -61,13 +61,6 @@ void LLL(double delta, int dim, double (*A)[dim], ...) {
       B[i][k] = vector[k]; // initialise to be the same as A
       } 
   }
-  printf("A after initialisation:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("A: %.4f\t", A[i][j]);
-        }
-        printf("\n");
-    }
   GramSchmidt(dim, 0, B); //GramSchmidt B
   
   k = 1;
@@ -75,21 +68,11 @@ void LLL(double delta, int dim, double (*A)[dim], ...) {
   double mu_kj;
   double mu_k_kminus1; //= InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]);
   while (k<dim) {
-    printf("k: %d\n", k);
     for (j=k-1; j>=0; j--) {
       mu_kj = InnerProduct(dim, A[k], B[j])/InnerProduct(dim, B[j], B[j]); 
       if (fabs(mu_kj) > 0.5) {
         for (i=0; i<dim; i++) {
-          printf("A[k][i] before: %.4f\n", A[k][i]);
           A[k][i] -= round(mu_kj) * A[j][i];
-          printf("A[k][i] after: %.4f\n", A[k][i]);
-        }
-        printf("A after updating:\n");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                printf("A: %.4f\t", A[i][j]);
-            }
-            printf("\n");
         }
         update_matrices(dim, k, A, B);
       }
@@ -98,12 +81,8 @@ void LLL(double delta, int dim, double (*A)[dim], ...) {
     
     
     mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
-    printf("compare1: %.4f\n", InnerProduct(dim, B[k], B[k]));
-    printf("compare2: %.4f\n", ((delta - (mu_k_kminus1*mu_k_kminus1)) * InnerProduct(dim, B[k-1], B[k-1])));
     if (InnerProduct(dim, B[k], B[k]) > ((delta - (mu_k_kminus1*mu_k_kminus1)) * InnerProduct(dim, B[k-1], B[k-1]))) {
-      printf("yes\n");
       k+=1;
-      printf("k2: %d", k);
       mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
         }
     else {
@@ -113,13 +92,6 @@ void LLL(double delta, int dim, double (*A)[dim], ...) {
         A[k][i] -= A[k-1][i];
       }
       update_matrices(dim, k, A, B); 
-      printf("A after updating again:\n");
-      for (int i = 0; i < 3; i++) {
-          for (int j = 0; j < 3; j++) {
-              printf("A: %.4f\t", A[i][j]);
-          }
-          printf("\n");
-      }
       k = fmax(k-1, 1);
       mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]);           
     }
