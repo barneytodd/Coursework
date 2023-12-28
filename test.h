795bf67d33d0cdbd61d92326e724bb207a7eb636
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-28 12:35:38 +0000

    Update test.h

--- test.h
+++ test.h
@@ -1,3 +1,7 @@
-double SumArray(int dim, double arr[dim])
+double SumArray(int dim, double arr[dim]);
 
-double Determinant(int dim, double (*A)[dim])
+double Determinant(int dim, double (*A)[dim]);
+
+double LimitCalc(int dim, double (*A)[dim]); 
+
+void runTests(int dim, ...);
