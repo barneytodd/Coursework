8207d7cdd285e4be61fd1fed4214942d6abdb6cd
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-29 22:59:49 +0000

    Update main.c

--- main.c
+++ main.c
@@ -52,8 +52,8 @@ int main(int argc, char *argv[]) {
         }
         printf("\n");
     }
-  double shortest_length = ShortestVector(3, A);
-  FILE *result = fopen("result.txt", "w");
-  fprintf(result, "%.4f\n", shortest_length);
+  //double shortest_length = ShortestVector(3, A);
+  //FILE *result = fopen("result.txt", "w");
+  //fprintf(result, "%.4f\n", shortest_length);
   return 0;
 }
