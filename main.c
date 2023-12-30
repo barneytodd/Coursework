97de29691aee632b752c95aeed8312e911a69e55
barneytodd <72504658+barneytodd@users.noreply.github.com>
2023-12-30 12:11:19 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 #include "LLL_Reduction.h"
 #include "Enumeration.h"
-#include "test.h"
+//#include "test.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+double SumArray(int dim, double arr[dim]) {
+    int i;
+    double sum1;
+    for (i=0; i<dim; i++) {
+        sum1 += arr[i];
+    }
+    return sum1;
+}
+
+double Determinant(int dim, double (*A)[dim]) {
+    int i, j, k;
+    double sum1;
+    bool skip[2] = {false, false};
+    double B[dim-1][dim-1];
+    if (dim == 1) {
+        return A[0][0];
+    }
+    for (i=0; i<dim; i++) {
+        skip[0] = skip[1] = false;
+        for (j=0; j<dim; j++) {
+            if (j==i) {
+                skip[0] = true;
+            }
+            for (k=0; k<dim; k++) {
+                if (k==i) {
+                    skip[1] = true;
+                }
+                B[j][k] = A[j+skip[0]][k+skip[1]];
+            }
+        }
+        sum1 += pow(-1, i) * A[0][i] * Determinant(dim-1, B);
+    }
+    return sum1;
+}
+
+double LimitCalc(int dim, double (*A)[dim]) {
+    double gamma = tgamma(dim/2 + 1);
+    double det = Determinant(dim, A);
+    return 1.05*(pow(gamma, 1/dim)/sqrt(M_PI))*pow(det, 1/dim);
+}
 
 int main(int argc, char *argv[]) {
   int dim, i, j, k;
