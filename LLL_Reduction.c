bb0a4abac064f4f6520f803d1faf9d653b2fa392
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-03 01:10:01 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "LLL_Reduction.h"
 
+//compute the inner product between two vectors
 double InnerProduct(int dim, double *arr1, double *arr2) {
   double sum1 = 0;
   int i;
@@ -12,78 +13,71 @@ double InnerProduct(int dim, double *arr1, double *arr2) {
   return sum1;
 }
 
+//compute GramSchmidt orthogonalisation without normalisation
 void GramSchmidt(int dim, int start, double B[][dim]) {
-  int i, j, k; //initialise variables i, j, k
+  int i, j, k; 
   double mu_ij;
-  double vec1[dim];
+  double vec1[dim]; //store values to subtract from initial vectors
   
-  for (i=start; i<dim; i++) { //iterate through the variables (vectors)
+  //iterate through the initial vectors
+  for (i=start; i<dim; i++) { 
     for (j=0; j<dim; j++) {
       vec1[j] = 0;
     }
-    for (j=0; j<i; j++) { //iterate through the previous vectors
+    //iterate through the previous vectors
+    for (j=0; j<i; j++) { 
       mu_ij = InnerProduct(dim, B[i], B[j])/InnerProduct(dim, B[j], B[j]);
       for (k=0; k<dim; k++) {
-        //if (k==0 && mu_ij == (B[i][0] * B[j][0])/(B[j][0] * B[j][0])) {
-          //printf("B[i][0]: %.4f\n", B[i][0]);
-         // vec1[k] += B[i][0];
-          //continue;
-        //}
-        vec1[k] += mu_ij * B[j][k]; //subtract the dot_product times the jth normalised vector 
+        vec1[k] += mu_ij * B[j][k]; //add the dot_product times the jth normalised vector 
       }
       
     }
+    //subtract from the ith initial vector
     for (k=0; k<dim; k++) {
       B[i][k] -= vec1[k];
     }
-    
   }
-  //printf("B1:\n");
-    //for (int i = 0; i < dim; i++) {
-      //  for (int j = 0; j < dim; j++) {
-        //    printf("%.4f\t", B[i][j]);
-        //}
-        //printf("\n");
-    //}
 }
   
-
+//when A gets updated, recompute B to be the GramSchmidt orthogonalised version of the updated A
 void update_matrices(int dim, int start, double **A, double B[][dim]) {
-  
   int i, j;
+
+  //set B to equal A
   for (i=0; i<dim; i++) {
     for (j=0; j<dim; j++) {
       B[i][j] = A[i][j];  
     }
   }
+  
   GramSchmidt(dim, start, B);
 }
 
 
-
+/// Lenstra–Lenstra–Lovász reduce the input matrix A
 void LLL(double delta, int dim, double **A) {
   
   int i, j, k; //initialise variables i, j, k
   double B[dim][dim];
   
-  for (i=0; i<dim; i++) { //iterate through the variables (vectors)
-    
+  //set be to be equal to A
+  for (i=0; i<dim; i++) { 
     for (j=0; j<dim; j++) { 
-      
-      B[i][j] = A[i][j]; //vector[k]; // initialise to be the same as A
+      B[i][j] = A[i][j]; 
       } 
   }
-  GramSchmidt(dim, 0, B); //GramSchmidt B
 
-  
-
-
+  //GramSchmidt orthogonslise B
+  GramSchmidt(dim, 0, B); 
   
   k = 1;
   int m = 0;
   double mu_kj;
-  double mu_k_kminus1; //= InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]);
+  double mu_k_kminus1; 
+
+  //
   while (k<dim) {
+    
     for (j=k-1; j>=0; j--) {
       mu_kj = InnerProduct(dim, A[k], B[j])/InnerProduct(dim, B[j], B[j]); 
       if (fabs(mu_kj) > 0.5) {
@@ -95,16 +89,13 @@ void LLL(double delta, int dim, double **A) {
       }
     }
 
-    
-
-    
-    
     mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
     if (InnerProduct(dim, B[k], B[k]) > ((delta - (mu_k_kminus1*mu_k_kminus1)) * InnerProduct(dim, B[k-1], B[k-1]))) {
       k+=1;
-      mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
+      //mu_k_kminus1 = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
         }
     else {
+      //swap A[k] and A[k-1]
       for (i=0; i<dim; i++) {
         A[k][i] += A[k-1][i];
         A[k-1][i] = A[k][i] - A[k-1][i];
