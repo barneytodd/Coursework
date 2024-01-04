eb1f000b4dcaf921e1fc968cb87f376c68f3ca01
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-04 17:08:39 +0000

    Update Threads.h

--- Threads.h
+++ Threads.h
@@ -1,6 +1,18 @@
 #ifndef THREADS_H
 #define THREADS_H
 
+#include <pthread.h>
+
+struct ThreadArgs {
+  int num;
+  int dim;
+  double *GS_norms;
+  double *Mu;
+  double *shortest_vector;
+  int *max_num;
+	pthread_mutex_t *lock;
+};
+
 void *Enumerate(void *args);
 
 double ShortestVector(int dim, double **A);
