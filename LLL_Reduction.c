363b378cf4137374b66c31a8d98088b86df5ea8c
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-05 13:22:15 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -45,7 +45,7 @@ void GramSchmidt(int dim, int start, double B[][dim]) {
       }
       mu_ij = InnerProduct(dim, B[i], B[j])/InnerProduct(dim, B[j], B[j]);
       for (k=0; k<dim; k++) {
-        vec1[k] += mu_ij * B[j][k]; * mag1; //add the dot_product times the jth normalised vector 
+        vec1[k] += mu_ij * B[j][k] * mag1; //add the dot_product times the jth normalised vector 
         
         B[j][k]*=mag2;
       }
