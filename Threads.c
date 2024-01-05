344d73feaa5d4a61afa47e1c4bc4c5c4115a4f0c
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-05 13:41:59 +0000

    Update Threads.c

--- Threads.c
+++ Threads.c
@@ -176,6 +176,9 @@ double ShortestVector1(int dim, double **A) {
 	//double sum2; //stores the sum of x[j] * Mu[j][i] for j>i
 	//double sum3; //sum of l[j]'s, (the sum of all the l[j]'s from 1 to dim is equal to the squared norm of the current combination of basis vectors)
 	//i=dim-1; //start with the last vector
+	printf("GS: %.4f\n", pow(GS_norms[dim-1], 0.5));
+	printf("ratio: %.4f\n", shortest_vector/pow(GS_norms[dim-1], 0.5));
+	printf("floor ratio: %d\n", floor(shortest_vector/pow(GS_norms[dim-1], 0.5)));
 	int max_num = floor(shortest_vector/pow(GS_norms[dim-1], 0.5));
 	pthread_t threads[max_num];
 	printf("%d\n", max_num);
