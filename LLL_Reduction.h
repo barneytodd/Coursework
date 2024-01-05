f68ae562a492d2d2941587469644f32cc4261ade
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-05 20:38:57 +0000

    Update LLL_Reduction.h

--- LLL_Reduction.h
+++ LLL_Reduction.h
@@ -9,4 +9,6 @@ void update_matrices(int dim, int start, double **A, double **B, double *Mu);
 
 double InnerProduct(int dim, double *arr1, double *arr2);
 
+bool CheckOrth(int dim, int start, double **B, bool *orth_check);
+
 #endif
