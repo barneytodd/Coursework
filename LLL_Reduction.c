1121ec971cf781174fd1c0371a91cf3eb1ef5664
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-06 17:52:30 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -5,24 +5,24 @@
 #include <stdbool.h>
 #include <limits.h>
 
-double Multiply(double *num1, double *num2) {
+double Multiply(double num1, double num2) {
 	printf("%.4f\n", *num1);
-	if (*num1<pow(10, 15) && *num2<pow(10, 15)) {
-		return (*num1)*(*num2);
+	if (num1<pow(10, 15) && num2<pow(10, 15)) {
+		return (num1)*(num2);
 	}
 	else {
 		int i, j;
 		char *str1 = (char *)malloc(16*sizeof(char));
 		char *str2 = (char *)malloc(1*sizeof(char));
-		int len1 = snprintf(str1, 16, "%.0f", fmax(*num1, *num2));
-		int len2 = snprintf(str1, 4, "%.0f", fmin(*num1, *num2));
+		int len1 = snprintf(str1, 16, "%.0f", fmax(num1, num2));
+		int len2 = snprintf(str1, 4, "%.0f", fmin(num1, num2));
 		if (len1 > 16) {
 			str1 = (char *)realloc(str1, (len1+1) * sizeof(char));
-        		snprintf(str1, len1+1, "%.0f", fmax(*num1, *num2)); 
+        		snprintf(str1, len1+1, "%.0f", fmax(num1, num2)); 
 		}
 		if (len2 > 2) {
 			str2 = (char *)realloc(str2, (len2+1) * sizeof(char));
-        		snprintf(str2, len2+1, "%.0f", fmin(*num1, *num2)); 
+        		snprintf(str2, len2+1, "%.0f", fmin(num1, num2)); 
 		}
 		char *result = (char *)malloc((len1+len2+2)*sizeof(char));
 		for (i=0; i<len1+len2+1; i++) {
