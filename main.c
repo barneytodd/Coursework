511d41b9863de1c5df8fccef53c6d3327eba9f0d
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-06 16:09:09 +0000

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char **argv) {
   int dim, i, j, k;
-	double a = 9223372036854775807.35;
+	double a = 9121212121212898989898989121212121212898989898989812121212121;
 	printf("%.4f\t %.4f\n", a, a*a);
 	exit(1);
   //printf("You have entered %d arguments:\n", argc);
