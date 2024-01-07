d1bcf1d3821256f1139a6316eea53e886bff6238
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-07 18:27:55 +0000

    Update GeneralFunctions.c

--- GeneralFunctions.c
+++ GeneralFunctions.c
@@ -1,19 +1,3 @@
-void FreeMemory(int dim, double **A, double **B, double *Mu) {
-	int i;
-	for (i=0; i<dim; i++) {
-		free(A[i]);
-		A[i] = NULL;
-		free(B[i]);
-		B[i] = NULL;
-	}
-	free(A);
-	A = NULL;
-	free(B);
-	B = NULL;	
-	free(Mu);
-	Mu = NULL;
-}
-
 void FreeMemoryA(int dim, double **A) {
 	int i;
 	for (i=0; i<dim; i++) {
@@ -24,14 +8,26 @@ void FreeMemoryA(int dim, double **A) {
 	A = NULL;
 }
 
-void FreeMemoryB(int dim, double **A) {
-	int i;
-	for (i=0; i<dim; i++) {
-		free(B[i]);
-		B[i] = NULL;
-	}
-	free(B);
+void FreeMemoryMu(int dim, double *Mu) {
+	free(Mu);
 	B = NULL;
+}
+
+void FreeMemory(int dim, int *choose_matrix, ...) { //double **A, double **B, double *Mu) {
+	va_list ap;
+	va_start (ap, choose_matrix);
+	while (*choose_matrix > 0) {
+		switch(*choose_matrix) {
+			case 1:
+				FreeMemoryA(dim, va_arg(ap, double **));
+				choose_matrix++;
+				break;
+			case 2:
+				FreeMemoryMu(dim, va_arg(ap, double *));
+				choose_matrix++;
+				break;
+		}
+	}
 }
 
 
