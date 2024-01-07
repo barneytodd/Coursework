9cf2c1025db26005adf890d6e6816e69117a2c81
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-07 18:42:57 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -5,43 +5,6 @@
 #include <stdbool.h>
 #include <limits.h>
 
-void FreeMemory(int dim, double **A, double **B, double *Mu) {
-	int i;
-	for (i=0; i<dim; i++) {
-		free(A[i]);
-		A[i] = NULL;
-		free(B[i]);
-		B[i] = NULL;
-	}
-	free(A);
-	A = NULL;
-	free(B);
-	B = NULL;	
-	free(Mu);
-	Mu = NULL;
-}
-
-void FreeMemoryA(int dim, double **A) {
-	int i;
-	for (i=0; i<dim; i++) {
-		free(A[i]);
-		A[i] = NULL;
-	}
-	free(A);
-	A = NULL;
-}
-
-void FreeMemoryB(int dim, double **A) {
-	int i;
-	for (i=0; i<dim; i++) {
-		free(B[i]);
-		B[i] = NULL;
-	}
-	free(B);
-	B = NULL;
-}
-
-
 //compute the inner product between two vectors
 double InnerProduct(int dim, double *arr1, double *arr2) {
   double sum1 = 0;
