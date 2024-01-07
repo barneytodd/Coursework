fb4891da7df27681f7dfb1c0c21ceb23c57b43dd
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-07 19:05:59 +0000

    Update GeneralFunctions.c

--- GeneralFunctions.c
+++ GeneralFunctions.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void FreeMatrix(int dim, double ***matrix) {
+  for (int i = 0; i<dim; i++) {
+    free((*matrix)[i]);
+    (*matrix)[i] = NULL;
+  }
+  free(*matrix);
+  *matrix = NULL;
+}
+
+
+//compute the inner product between two vectors
+double InnerProduct(int dim, double *arr1, double *arr2) {
+  double sum1 = 0;
+  int i;
+  for (i=0; i<dim; i++) {
+    sum1 += arr1[i]*arr2[i];
+  }
+  return sum1;
+}
