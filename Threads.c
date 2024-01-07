c2957d954a38e2d9e61b48fc1b71effb30f827a6
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-07 00:11:39 +0000

    Update Threads.c

--- Threads.c
+++ Threads.c
@@ -148,6 +148,7 @@ double ShortestVector1(int dim, double **A, double **B, double *Mu) {
 		GS_norms[i] = InnerProduct(dim, B[i], B[i]);
 	}
 
+	printf("GS\n");
 	int max_num = floor(shortest_vector/pow(GS_norms[dim-1], 0.5)); //maximum possible value for x[dim-1]
 	pthread_t threads[max_num];
 
@@ -157,7 +158,7 @@ double ShortestVector1(int dim, double **A, double **B, double *Mu) {
 		printf("Error: Mutex initialization failed\n");
 		exit(1);
   }
-
+	printf("max_num: %d\n", max_num);
 	//divide the enumeration into threads by x[dim-1] value
 	struct ThreadArgs args[max_num+1];
 	int count = 0;
