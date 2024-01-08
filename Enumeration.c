44d665c0b6b4073ba31c764c1ab24ce6bab7896c
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-08 13:46:12 +0000

    Update Enumeration.c

--- Enumeration.c
+++ Enumeration.c
@@ -33,6 +33,7 @@ void *Enumerate(void *args) {
 	printf("start enum loop\n");
 	for (j=0; j<thread_args->dim; j++) {
 		for (k=0; k<thread_args->dim; k++) {
+			printf("i ");
 			printf("%.4f ", *(thread_args->Mu)[(i-1)*i/2+j]);
 		}
 		printf("\n");
