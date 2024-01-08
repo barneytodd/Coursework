2d54ad167aeb02f9a20fab88fbd84767e55abe66
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-08 17:36:51 +0000

    Update test.c

--- test.c
+++ test.c
@@ -177,7 +177,7 @@ int main() {
     //set the bounds for the values of the second input matrix, and its dimension
     int min = -10000;
     int max = 10000;
-    dim = 80;
+    dim = 60;
 
     //resize A, B and Mu
     A = realloc(A, dim * sizeof(double *));
