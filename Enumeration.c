6d41f052dca02bd1bad61ec9b3c050e0448a919f
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-08 17:01:55 +0000

    Update Enumeration.c

--- Enumeration.c
+++ Enumeration.c
@@ -9,18 +9,12 @@ void *Enumerate(void *args) {
 	
 	//restructure the arguments into the form of the struct above
   struct ThreadArgs *thread_args = (struct ThreadArgs *)args;
-	if ((*(thread_args->GS_norms))[0] < pow((*thread_args->B)[0][0], 0.5)) {
-		printf("yes\n");
-		//thread_args->GS_norms[0] = thread_args->GS0;
-	}
   int x[thread_args->dim], i, j, k; //x stores the number of each basis vector used to reach each lattice point
   double l[thread_args->dim]; //stores the total contribution squared, of the combination of basis vectors stored in x, in the direction of the ith GS vector
 	for (i=0; i<thread_args->dim; i++) {
 		printf("%.4f ", (*(thread_args->GS_norms))[i]);
 	}
 	printf("\n");
-	//printf("gs: %.4f\n", thread_args->GS0);
-	exit(1);
   for (i=0; i<thread_args->dim-1; i++) {
     x[i] = 0;
   }
@@ -49,7 +43,7 @@ void *Enumerate(void *args) {
 			for (k=j+1; k<thread_args->dim; k++) {
 				sum2 += x[k] * thread_args->Mu[(k-1)*k/2+j]; 
 			}
-			//l[j] = (x[j] + sum2) * (x[j] + sum2) * thread_args->GS_norms[j]; 	
+			l[j] = (x[j] + sum2) * (x[j] + sum2) * (*(thread_args->GS_norms))[j]; 	
 		}
 		printf("1\n");
 		//sum the l[j] values for j>=i
@@ -84,7 +78,7 @@ void *Enumerate(void *args) {
 					sum2 += x[k] * thread_args->Mu[(k-1)*k/2+i]; 
 				}
 				x[i] = round(- sum2); //the integer which minimises l[i], if this doesn't work then no other integer will
-				//l[i] = ((double)x[i] + sum2) * ((double)x[i] + sum2) * thread_args->GS_norms[i]; 
+				l[i] = ((double)x[i] + sum2) * ((double)x[i] + sum2) * (*(thread_args->GS_norms))[i]; 
 				printf("3\n");
 				if (l[i] < (*(thread_args->shortest_vector)) * (*(thread_args->shortest_vector)) - sum3) {
 					//subtract 1 from x[i] until l[i] is no longer < shortest_vector^2 - sum3
@@ -97,7 +91,7 @@ void *Enumerate(void *args) {
 							sum2 += x[k] * thread_args->Mu[(k-1)*k/2+i];
 						}
 						printf("sum2: %.4f\n", sum2);
-						//l[i] = (x[i] + sum2) * (x[i] + sum2) * thread_args->GS_norms[i]; 
+						l[i] = (x[i] + sum2) * (x[i] + sum2) * (*(thread_args->GS_norms))[i]; 
 						//printf("%.4f\n", thread_args->GS_norms[i]);
 						printf("l[i]: %.4f\n", l[i]);
 						printf("x[i]: %d\n", x[i]);
@@ -121,7 +115,7 @@ void *Enumerate(void *args) {
 				printf("7\n");
 				short_vec = *(thread_args->shortest_vector);
 				
-				//l[thread_args->dim-2] = pow(x[thread_args->dim-2] + x[thread_args->dim-1] * thread_args->Mu[(k-1)*k/2+j], 2)*thread_args->GS_norms[thread_args->dim-2]; 
+				l[thread_args->dim-2] = pow(x[thread_args->dim-2] + x[thread_args->dim-1] * thread_args->Mu[(k-1)*k/2+j], 2)* (*(thread_args->GS_norms))[thread_args->dim-2]; 
 
 				//if l[dim-2] + l[dim-1] < shortest_vector^2, then we are fine to carry on
 				if (l[thread_args->dim-2]+l[thread_args->dim-1] < pow(*(thread_args->shortest_vector), 2)) {
@@ -129,19 +123,19 @@ void *Enumerate(void *args) {
 				}
 				//if l[dim-2] + l[dim-1] > shortest_vector^2, then x[dim-2] is now out of range w.r.t. the new shortest_vector
 				//this could potentially lead to this thread terminating before it has checked all possible x values
-				//else {
+				else {
 					//if l[dim-2] calculated with x[dim-2] < l[dim-2] calculated with x[dim-2]-1, then x[dim-2] is below the new accepted range
 					//therefore we haven't yet checked the x[dim2] values in the new accepted range, so we reset i to dim-1 and carry on
-					//if (l[thread_args->dim-2] < pow((x[thread_args->dim-2]-1) + (x[thread_args->dim-1]-1) * thread_args->Mu[(k-1)*k/2+j], 2)*thread_args->GS_norms[thread_args->dim-2]) {
-					//	i = thread_args->dim-1;
-					//	continue;
-					//}
+					if (l[thread_args->dim-2] < pow((x[thread_args->dim-2]-1) + (x[thread_args->dim-1]-1) * thread_args->Mu[(k-1)*k/2+j], 2)* (*(thread_args->GS_norms))[thread_args->dim-2]) {
+						i = thread_args->dim-1;
+						continue;
+					}
 					//in the opposite case, x[dim-2] is above the new accepted range, and so we have already checked all possibilities in this new range
 					//therefore we terminate this thread
-					//else {
-					//	break;
-					//}
-				//}
+					else {
+						break;
+					}
+				}
 			}
 			//otherwise we proceed as normal
 			i++;
