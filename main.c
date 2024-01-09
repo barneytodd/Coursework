7dbabcf463e3ae53e1b99f98174978e4df2da8e8
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-09 14:58:07 +0000

    Update main.c

--- main.c
+++ main.c
@@ -163,7 +163,7 @@ int main(int argc, char **argv) {
 	}
 
   //reduce the lattice basis using Lenstra–Lenstra–Lovász lattice reduction
-  LLL(0.75, dim, A, B, Mu);
+  //LLL(0.75, dim, A, B, Mu);
 	
   printf("Orthonormalized Vectors (A):\n");
   printf("[");
