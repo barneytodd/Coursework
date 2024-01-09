b36c16de43156faccca159acba6be81d6b3ae5ff
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-09 14:55:59 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -8,36 +8,48 @@
 //compute GramSchmidt orthogonalisation without normalisation
 void GramSchmidt(int dim, int start, double **B, double *Mu) {
   int i, j, k; 
-  double vec1[dim]; //store values to subtract from initial vectors
+  //double vec1[dim]; //store values to subtract from initial vectors
   double mag1; //stores the norm of each i vector in the following loop
   double mag2; //stores the norm of each j vector
 	
 	//iterate through the initial vectors
 	for (i=fmax(start, 1); i<dim; i++) { 
 		mag1 = sqrt(InnerProduct(dim, B[i], B[i]));
-		for (j=0; j<dim; j++) {
-			vec1[j] = 0;
-			B[i][j] /= mag1; //normalising vectors before computing inner proucts helps to reduce inaccuracies caused by double calculations
+		for (k=0; k<dim; k++) {
+			//vec1[k] = 0;
+			B[i][k] /= mag1; //normalising vectors before computing inner proucts helps to reduce inaccuracies caused by double calculations
 		}
-		//iterate through the previous vectors
-		for (j=0; j<i; j++) { 
+		for (j=0; j<i; j++) {
 			mag2 = sqrt(InnerProduct(dim, B[j], B[j]));
 			for (k=0;k<dim;k++) {
 		  	B[j][k] /= mag2; //normalise before inner product
 			}
-			Mu[(i-1)*i/2+j] = InnerProduct(dim, B[i], B[j])*mag1;///InnerProduct(dim, B[j], B[j]);
+			Mu[(i-1)*i/2+j] = InnerProduct(dim, B[i], B[j])*mag1;
+		}
+		for (k=0; k<dim; k++) {
+			B[i][k] *= mag1
+		}
+		//iterate through the previous vectors
+		for (j=0; j<i; j++) { 
+			//mag2 = sqrt(InnerProduct(dim, B[j], B[j]));
+			//for (k=0;k<dim;k++) {
+		  	//B[j][k] /= mag2; //normalise before inner product
+			//}
+			//Mu[(i-1)*i/2+j] = InnerProduct(dim, B[i], B[j])*mag1;///InnerProduct(dim, B[j], B[j]);
 			for (k=0; k<dim; k++) {
-		  	vec1[k] += Mu[(i-1)*i/2+j] * B[j][k]; //add the dot_product times the jth normalised vector 
-				B[j][k]*=mag2; //reset B[j] to original values
+		  		//vec1[k] += Mu[(i-1)*i/2+j] * B[j][k]; //add the dot_product times the jth normalised vector 
+				
+				B[i][k] -= Mu[(i-1)*i/2+j] * B[j][k];
+				B[j][k] *= mag2; //reset B[j] to original values
 			} 
 			Mu[(i-1)*i/2+j] /= mag2; //reset Mu[i][j] to normal value     
 		}
 				
 		//subtract from the ith initial vector
-		for (k=0; k<dim; k++) {
-		B[i][k]*=mag1; //reset B[i] to original version
-		B[i][k] -= vec1[k];
-		}
+		//for (k=0; k<dim; k++) {
+			//B[i][k]*=mag1; //reset B[i] to original version
+			//B[i][k] -= vec1[k];
+		//}
 	}
 }
   
