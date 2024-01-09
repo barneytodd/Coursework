afe6dad5a9c4aa40aa54088bbd4588c877db251a
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-09 12:34:32 +0000

    Update LLL_Reduction.c

--- LLL_Reduction.c
+++ LLL_Reduction.c
@@ -6,86 +6,43 @@
 #include <stdbool.h>
 #include <limits.h>
 
-//checks if GS was successful, for large numbers floating point inaccuracy can cause GS vectors to not be orthogonal
-//requires cos(theta)<0.0001 for all pairs of GS vectors
-bool CheckOrth(int dim, int start, double **B) {
-  int i, j, k;
-  double mag1;
-  double mag2;
-  for (i=fmax(start, 1); i<dim; i++) {
-      mag1 = sqrt(InnerProduct(dim, B[i], B[i]));
-      for (k=0; k<dim; k++) {
-        B[i][k] /= mag1;
-      }
-      for (j=0; j<i; j++) {
-        mag2 = sqrt(InnerProduct(dim, B[j], B[j]));
-        for (k=0; k<dim; k++) {
-          B[j][k] /= mag2;
-        }
-        if (fabs(InnerProduct(dim, B[i], B[j])) > 0.0001) {
-					for (k=0; k<dim; k++) {
-						B[j][k] *= mag2;
-						B[i][k] *= mag1;
-					}
-					return false;
-        }
-        for (k=0; k<dim; k++) {
-        B[j][k] *= mag2;
-        }
-      }
-      for (k=0; k<dim; k++) {
-        B[i][k] *= mag1;
-      } 
-    }
-	return true;
-}
-
 //compute GramSchmidt orthogonalisation without normalisation
 void GramSchmidt(int dim, int start, double **B, double *Mu) {
   int i, j, k; 
   double vec1[dim]; //store values to subtract from initial vectors
-  double mag1;
-  double mag2;
+  double mag1; //stores the norm of each i vector in the following loop
+  double mag2; //stores the norm of each j vector
 	
-  bool orth_check = false;
-  while (!orth_check) {
-    orth_check = true;
-	
-    //iterate through the initial vectors
-    for (i=fmax(start, 1); i<dim; i++) { 
-      mag1 = sqrt(InnerProduct(dim, B[i], B[i]));
-      for (j=0; j<dim; j++) {
-        vec1[j] = 0;
-        B[i][j] /= mag1; //normalising vectors before computing inner proucts helps to reduce inaccuracies caused by double calculations
-      }
-      //iterate through the previous vectors
-      for (j=0; j<i; j++) { 
-        mag2 = sqrt(InnerProduct(dim, B[j], B[j]));
-        for (k=0;k<dim;k++) {
-          B[j][k] /= mag2; //normalise before inner product
-        }
-        
-        Mu[(i-1)*i/2+j] = InnerProduct(dim, B[i], B[j])*mag1;///InnerProduct(dim, B[j], B[j]);
-        for (k=0; k<dim; k++) {
-          vec1[k] += Mu[(i-1)*i/2+j] * B[j][k]; //add the dot_product times the jth normalised vector 
-					B[j][k]*=mag2; //reset B[j] to original values
-        } 
-				Mu[(i-1)*i/2+j] /= mag2; //reset Mu[i][j] to normal value     
-      }
-			
-      //subtract from the ith initial vector
-      for (k=0; k<dim; k++) {
-        B[i][k]*=mag1; //reset B[i] to original version
-        B[i][k] -= vec1[k];
-      }
-    }
-	
-	  orth_check = CheckOrth(dim, start, B);
-	
-  }
+	//iterate through the initial vectors
+	for (i=fmax(start, 1); i<dim; i++) { 
+		mag1 = sqrt(InnerProduct(dim, B[i], B[i]));
+		for (j=0; j<dim; j++) {
+			vec1[j] = 0;
+			B[i][j] /= mag1; //normalising vectors before computing inner proucts helps to reduce inaccuracies caused by double calculations
+		}
+		//iterate through the previous vectors
+		for (j=0; j<i; j++) { 
+			mag2 = sqrt(InnerProduct(dim, B[j], B[j]));
+			for (k=0;k<dim;k++) {
+		  	B[j][k] /= mag2; //normalise before inner product
+			}
+			Mu[(i-1)*i/2+j] = InnerProduct(dim, B[i], B[j])*mag1;///InnerProduct(dim, B[j], B[j]);
+			for (k=0; k<dim; k++) {
+		  	vec1[k] += Mu[(i-1)*i/2+j] * B[j][k]; //add the dot_product times the jth normalised vector 
+				B[j][k]*=mag2; //reset B[j] to original values
+			} 
+			Mu[(i-1)*i/2+j] /= mag2; //reset Mu[i][j] to normal value     
+		}
+				
+		//subtract from the ith initial vector
+		for (k=0; k<dim; k++) {
+		B[i][k]*=mag1; //reset B[i] to original version
+		B[i][k] -= vec1[k];
+		}
+	}
 }
   
-//when A gets updated, recompute B to be the GramSchmidt orthogonalised version of the updated A
+//when A gets updated, recompute B to be the GramSchmidt orthogonalised version of the updated A, along with the associated Mu values
 void update_matrices(int dim, int start, double **A, double **B, double *Mu) {
   int i, j;
   //set B to equal A for the vectors after the one that has just changed
@@ -94,7 +51,6 @@ void update_matrices(int dim, int start, double **A, double **B, double *Mu) {
       B[i][j] = A[i][j];  
     }
   }
-  
   GramSchmidt(dim, start, B, Mu);
 }
 
@@ -148,11 +104,13 @@ void LLL(double delta, int dim, double **A, double **B, double *Mu) {
 				m=0;
       }
     }
+		
     //LLL basis reduction requires (B[k] . B[k]) > (delta - mu_k_k-1) * (B[k-1] . B[k-1]) for every k
     Mu[(k-1)*k/2+k-1] = InnerProduct(dim, A[k], B[k-1])/InnerProduct(dim, B[k-1], B[k-1]); 
     if (InnerProduct(dim, B[k], B[k]) > ((delta - (Mu[(k-1)*k/2+k-1]*Mu[(k-1)*k/2+k-1])) * InnerProduct(dim, B[k-1], B[k-1]))) {
       k+=1;
         }
+			
     else {
       //swap A[k] and A[k-1]
       for (i=0; i<dim; i++) {
