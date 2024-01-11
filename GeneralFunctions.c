c5aca808550bab69abd0e67c72de84419bb02d70
barneytodd <72504658+barneytodd@users.noreply.github.com>
2024-01-11 14:52:41 +0000

    Update GeneralFunctions.c

--- GeneralFunctions.c
+++ GeneralFunctions.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//used to free the memory allocated for A and B when either an error arises or the end of the program is reached
+// used to free the memory allocated for A and B when either an error arises or the end of the program is reached
 void FreeMatrix(int dim, double ***matrix) {
-  for (int i = 0; i<dim; i++) {
+  for (int i = 0; i < dim; i++) {
     free((*matrix)[i]);
     (*matrix)[i] = NULL;
   }
@@ -13,11 +13,11 @@ void FreeMatrix(int dim, double ***matrix) {
 }
 
 
-//compute the inner product between two vectors
+// compute the inner product between two vectors
 double InnerProduct(int dim, double *arr1, double *arr2) {
   double sum1 = 0;
   int i;
-  for (i=0; i<dim; i++) {
+  for (i=0; i < dim; i++) {
     sum1 += arr1[i]*arr2[i];
   }
   return sum1;
